e00eb28316ede89e59ff8c76b619282d28caa892
blakejones <119406401+KBlakeJones@users.noreply.github.com>
2023-04-24 17:39:57 -0400

    Add Do while

--- src/wrestlingGame/GameControllerMain.java
+++ src/wrestlingGame/GameControllerMain.java
@@ -8,19 +8,27 @@ public class GameControllerMain {
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Welcome to WrestleCrania! You'll have to use your brain to make it to the main event.");
-		System.out.println("Are you ready to start a game?");
-		String result = in.next();
-		result = (result.toLowerCase().substring(0, 1));
+//		System.out.println("Are you ready to start a game?");
+//		String result = in.next();
+//		result = (result.toLowerCase().substring(0, 1));
+		String result;
+		do {
+			System.out.println("Are you ready to start a game?");
+			result = in.next();
+			result = (result.toLowerCase().substring(0, 1));
 		if(result.equals("y")) {
 			System.out.println("Great, let's get you set up!");
-			
 		} else if (result.equals("n")) {//else if No then this
 			System.out.println("Come back when you're ready chump!");
-		}else 
+			continue;
+		} else 
 			System.out.println("Please enter yes or no");
+		}while (!result.equals("y"));
+		
+		//(!result.equals("y"));
 		//try catch with exceptions for incorrect input 
 		//if you enter 1 instead of yes or no it 
-		// possibly change to do while loop or if else if?
+		// possibly change to do while loop?
 		//don't let it pass else if you choose no
 		System.out.println("Choose a name for your wrestler:");
 		result = in.next();
@@ -49,10 +57,11 @@ public class GameControllerMain {
 		result = (result.toLowerCase().substring(0, 1));
 		if(result.equals("y")) {
 			System.out.println("Great, you have bookings for the following, please choose one");
-			
-		} else //else if No then this
+		} else if (result.equals("n")){//else if No then this
 			System.out.println("Come back when you're ready chump!");
-		
+		} else {
+			System.out.println("Please enter yes or no");
+		}
 		//add in if yes then this
 		//if no then loop to front come back when you're ready chump or another catch-phrase
 		
